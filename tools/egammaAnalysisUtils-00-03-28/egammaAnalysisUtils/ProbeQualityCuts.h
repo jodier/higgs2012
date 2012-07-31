c6f4d68b5d1683350afc0808270376cc90a1a112
Jérôme ODIER <odier.jerome@gmail.com>
2012-07-31 16:26:20 +0200

    update

--- tools/egammaAnalysisUtils-00-03-28/egammaAnalysisUtils/ProbeQualityCuts.h
+++
@@ -1,6 +0,0 @@
-#ifndef PROBEQUALITYCUTS_H
-#define PROBEQUALITYCUTS_H
-//-------------------------------------------------------------------------------------------------------
-bool passProbeQualityRHad(double eta, double eT,double rHad, double rHad1);
-//----------------------------------------------------------------------------------------
-#endif
